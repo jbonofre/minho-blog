f57f91b6c35294e866ab7f0a0144393206266fad
JB Onofré <jbonofre@apache.org>
2023-01-17 14:58:58 +0100

    Add colocation example with Spring Boot application

---
+++ my-spring-boot-app/src/main/java/net/nanthrax/blog/minho/myapp/FirstApp.java
@@ -0,0 +1,21 @@
+package net.nanthrax.blog.minho.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class FirstApp {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstApp.class, args);
+    }
+
+}
